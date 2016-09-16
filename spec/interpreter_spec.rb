a6e22dce2bd6d0d21d495ff6bccf56da42687694
Thomas E. Enebo <tom.enebo@gmail.com>
2016-09-16 07:41:42 -0500

    Start adding more specs to cover instructions and whether interpreter is working

--- spec/interpreter_spec.rb
+++ spec/interpreter_spec.rb
@@ -2,19 +2,54 @@ require 'rpiet/interpreter'
 require 'rpiet/image/ascii_image'
 
 describe "RPiet::Interpreter" do
-  let(:source) do
+  let(:push) do
     RPiet::Image::AsciiImage.new <<-EOS
 nb db ++
 nb ++ ++
 ++ ++ ++
     EOS
   end
-  it "Can push" do
-    interpreter = RPiet::Interpreter.new source
 
+    let(:push_pop) do
+      RPiet::Image::AsciiImage.new <<-EOS
+nb db nb ++
+nb ++ nb ++
+++ ++ ++ ++
+      EOS
+    end
+
+  let(:push_add) do
+    RPiet::Image::AsciiImage.new <<-EOS
+nb db lb lm ++
+nb ++ ++ lm ++
+++ ++ ++ ++ ++
+    EOS
+  end
+
+  it "Can push and terminate" do
+    interpreter = RPiet::Interpreter.new push
     interpreter.next_step
     expect(interpreter.pvm.stack).to eq [2]
     interpreter.next_step
     expect(interpreter.pvm.stack).to eq []
   end
+
+  it "Can push and pop" do
+    interpreter = RPiet::Interpreter.new push_pop
+    interpreter.next_step
+    expect(interpreter.pvm.stack).to eq [2]
+    interpreter.next_step
+    expect(interpreter.pvm.stack).to eq []
+  end
+
+  it "Can push and add" do
+    interpreter = RPiet::Interpreter.new push_add
+    interpreter.next_step
+    expect(interpreter.pvm.stack).to eq [2]
+    interpreter.next_step
+    expect(interpreter.pvm.stack).to eq [2, 1]
+    interpreter.next_step
+    expect(interpreter.pvm.stack).to eq [3]
+  end
+
 end
